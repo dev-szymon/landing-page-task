2281b244b7eee48bcddc9060511b886083a20ef4
Szymon Gazda <szymongazda95@gmail.com>
2024-03-10 16:43:17 +0100

    refactor: move gradient border from AdvertBar to Header

--- app/components/AdvertBar/AdvertBar.tsx
+++ app/components/AdvertBar/AdvertBar.tsx
@@ -8,16 +8,14 @@ const AdvertBar: React.FC<AdvertBarProps> = ({className}) => {
     return (
         <div
             className={clsx(
-                "w-full after:block after:h-2 after:w-full after:content-[''] after:bg-gradient-primary",
+                "flex h-15.5 w-full flex-col items-center justify-center bg-gray",
                 className
             )}
         >
-            <div className="flex h-15.5 w-full flex-col items-center justify-center bg-gray">
-                <p className="font-primary w-full text-center text-sm uppercase text-black-dim tracking-07">
-                    {"NEW CUSTOMERS GET 15% OFF! USE CODE "}
-                    <span className="text-blue-dim">WELCOME15</span>
-                </p>
-            </div>
+            <p className="w-full text-center font-primary text-sm uppercase text-black-dim tracking-07">
+                {"NEW CUSTOMERS GET 15% OFF! USE CODE "}
+                <span className="text-blue-dim">WELCOME15</span>
+            </p>
         </div>
     );
 };
--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -21,29 +21,32 @@ const Header: React.FC<HeaderProps> = ({className}) => {
     return (
         <header
             className={clsx(
-                "flex h-25 items-center justify-center border-b border-gray-dark bg-gray-light",
+                "sticky top-0 z-50 w-full border-b border-gray-dark bg-gray-light",
                 className
             )}
         >
-            <div className="flex w-full max-w-leet items-center justify-between gap-4 px-4">
-                <nav className="flex flex-1 justify-start gap-4">
-                    {navItems.map((item) => (
-                        <NavItem key={item.label} {...item} />
-                    ))}
-                </nav>
+            <div className="h-2 w-full bg-gradient-primary" />
+            <div className="flex h-25 items-center justify-center">
+                <div className="flex w-full max-w-leet items-center justify-between gap-4 px-4">
+                    <nav className="flex flex-1 justify-start gap-4">
+                        {navItems.map((item) => (
+                            <NavItem key={item.label} {...item} />
+                        ))}
+                    </nav>
 
-                <LogoSvg className="w-12 text-brown" />
+                    <LogoSvg className="w-12 text-brown" />
 
-                <div className="flex flex-1 justify-end gap-5">
-                    <HeaderButton>
-                        <CartSvg className="w-5" />
-                    </HeaderButton>
-                    <HeaderButton>
-                        <UserSvg className="w-5" />
-                    </HeaderButton>
-                    <HeaderButton>
-                        <SearchSvg className="w-5" />
-                    </HeaderButton>
+                    <div className="flex flex-1 justify-end gap-5">
+                        <HeaderButton>
+                            <CartSvg className="w-5" />
+                        </HeaderButton>
+                        <HeaderButton>
+                            <UserSvg className="w-5" />
+                        </HeaderButton>
+                        <HeaderButton>
+                            <SearchSvg className="w-5" />
+                        </HeaderButton>
+                    </div>
                 </div>
             </div>
         </header>
