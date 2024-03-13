c6b8916f716281e668e544b01d1e82273cdbf729
Szymon Gazda <szymongazda95@gmail.com>
2024-03-13 21:11:13 +0100

    chore: Add jsdoc to util functions

--- app/utils/displayDoubleDigit.ts
+++ app/utils/displayDoubleDigit.ts
@@ -1,3 +1,10 @@
+/**
+ * @param {nubmer} value number which will be converted into double digit string.
+ * @returns {string} string representation of input number always displayed as double digit.
+ *
+ * @example displayDoubleDigits(1) // `01`
+ * @example displayDoubleDigits(12) // `12`
+ */
 export const displayDoubleDigits = (value: number): string => {
     const roundedValue = Math.floor(value);
 
--- app/utils/noop.ts
+++ app/utils/noop.ts
@@ -1,3 +1,6 @@
-export const noop = () => {
+/**
+ * Utility no-op function. Useful for providing fallback, defaults or initial context state.
+ */
+export const noop = (): void => {
     return;
 };
